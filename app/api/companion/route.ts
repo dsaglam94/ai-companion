344dfba812c50676232875e990e6f03f608f6ac5
dsaglam94 <d.saglam@outlook.fr>
2023-08-27 16:31:52 +0300

    adding POST route to create companion

---
+++ app/api/companion/route.ts
@@ -0,0 +1,45 @@
+import prismadb from "@/lib/prismadb";
+
+import { currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const user = await currentUser();
+    const { src, name, description, instructions, seed, categoryId } = body;
+
+    if (!user || !user.id || !user.firstName) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (
+      !src ||
+      !name ||
+      !description ||
+      !instructions ||
+      !seed ||
+      !categoryId
+    ) {
+      return new NextResponse("Missing required fields!", { status: 400 });
+    }
+
+    const companion = await prismadb.companion.create({
+      data: {
+        src,
+        name,
+        description,
+        instructions,
+        seed,
+        categoryId,
+        userId: user.id,
+        userName: user.firstName,
+      },
+    });
+
+    return NextResponse.json(companion);
+  } catch (error) {
+    console.log("[Companion_Post] error:: ", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
